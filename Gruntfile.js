55f5351f39bfa4a522347af6753548972550fba6
Darrin Edelman <darrin@plexapp.com>
2014-11-12 15:34:44 -0600

    The cruft that nobody really cares about.

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    var jsFiles = ['Grunfile.js', '*.js', 'bin/*.js'];
+
+    // ===========================================================================
+    // CONFIGURE GRUNT ===========================================================
+    // ===========================================================================
+    grunt.initConfig({
+
+        // get the configuration info from package.json ----------------------------
+        // this way we can use things like name and version (pkg.name)
+        pkg: grunt.file.readJSON('package.json'),
+
+        nodeunit: {
+            all: ['tests/**/*.test.js'],
+            options: {
+                reporter: 'junit',
+                reporterOptions: {
+                    output: './build/reports/nodeunit/'
+                }
+            }
+        },
+
+        // configure jshint to validate js files -----------------------------------
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: jsFiles
+        },
+
+        watch: {
+            options: {
+                atBegin : true
+            },
+            jshint: {
+                files: [jsFiles],
+                tasks: ['jshint']
+            },
+            nodeunit: {
+                files: [jsFiles],
+                tasks: ['nodeunit']
+            }
+        }, // watch
+
+        concurrent: {
+            // Restart the server
+            dev: {
+                tasks: ['watch:jshint', 'watch:nodeunit'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        } // concurrent
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    
+    grunt.registerTask('ci', '', function () {
+        var taskList = [
+            'concurrent:dev'
+        ];
+        grunt.task.run(taskList);
+    });
+
+    grunt.registerTask('default', '', 'ci');
+};
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Introduction
+
+The bold and brave will use 'eval' to do the same thing this module does - creates a Regexp.
+
+Because this module is all about creating a regex from an input string - which could come from a user the use of eval in this context makes me nervous. So then - this module is for the weak of heart - like me who would prefer to avoid the potential of someone hijacking my software by way of some clever javascript introduced where a regexp was expected.
